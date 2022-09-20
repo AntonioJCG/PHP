b76582af073ad4ba3ae5b19b4ebed25b4ead75f9
AntonioJCG <antoniojesuscabrera6@gmail.com>
2022-09-20 11:33:27 +0200

    3.3

---
+++ Tema 3/Apartado 3.3/ejer3.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo "<table border='1'>";
+    echo "<tr>
+    <td>Castellano</td>
+    <td>123</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    </tr>
+    <tr>
+    <td>Inglés</td>
+    <td>123</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    </tr>";
+    echo "</table>";
+    ?>
+</body>
+
+</html>
