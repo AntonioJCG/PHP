d30c00fdf99a62d5417e1db0896f0a143678fffd
AntonioJCG <antoniojesuscabrera6@gmail.com>
2022-10-10 14:24:32 +0200

    Ejercicio

--- Tema5/Apartado5.8/ejer2.php
+++ Tema5/Apartado5.8/ejer2.php
@@ -10,36 +10,43 @@
 
 <body>
     <?php
-    $pedidoArray = [];
-    $pedido = base64_encode(serialize($pedidoArray));
-    if (isset($_REQUEST["finalizado"])) {
+    if (!isset($_POST["pedido"])) {
+        $pedido = [];
     } else {
-        if (isset($_REQUEST['pedido'])) {
-            $aux = unserialize(base64_decode($_REQUEST['pedido']));
-            $ingredientes = [$_REQUEST];
-            array_push($aux, $ingredientes);
-            foreach ($aux as $comida) {
-                 for ($i = 1; $i < count($comida) - 1; $i++) {
-                    echo $comida[$i];
-                 }
-            }
-            $pedido = base64_encode(serialize($aux));
-            echo $pedido;
-        }
+        $pedido = unserialize(base64_decode($_POST["pedido"]));
+        $pedido[] = $_POST["ingrediente"];
+    }
 
+    if (isset($_REQUEST["finalizado"])) {
+        
+    } else {
+        if (isset($_POST["anadir"])) {
+            $comida = $_POST["ingrediente"];
+            for ($i = 0; $i < count($comida); $i++) {
+                if ($i == 0) {
+                    echo "$comida[$i] de ";
+                } else {
+                    if ($i == count($comida) - 1) {
+                        echo " $comida[$i].";
+                    } else {
+                        echo " $comida[$i], ";
+                    }
+                }
+            }
+        }
     ?>
         <h2>Realiza tu pedido</h2>
         <form action="ejer2.php" method="get">
             <h3>Pizza</h3>
-            <input type="hidden" name="0" value="Pizza">
-            <input type="checkbox" name="1" value="atun"> Atún
-            <input type="checkbox" name="2" value="jamon"> Jamón
-            <input type="checkbox" name="3" value="olivas"> Olivas
-            <input type="checkbox" name="4" value="bacon"> Bacon
-            <input type="checkbox" name="5" value="peperoni"> Peperoni
-            <input type="checkbox" name="6" value="piña"> Piña
-            <input type="hidden" name="pedido" value="<?= $pedido ?>">
-            <br><br><input type="submit" value="Añadir">
+            <input type="hidden" name="ingrediente[]" value="Pizza">
+            <input type="checkbox" name="ingrediente[]" value="atun"> Atún
+            <input type="checkbox" name="ingrediente[]" value="jamon"> Jamón
+            <input type="checkbox" name="ingrediente[]" value="olivas"> Olivas
+            <input type="checkbox" name="ingrediente[]" value="bacon"> Bacon
+            <input type="checkbox" name="ingrediente[]" value="peperoni"> Peperoni
+            <input type="checkbox" name="ingrediente[]" value="piña"> Piña
+            <input type="hidden" name="pedido" value="<?= base64_encode(serialize($pedido)) ?>">
+            <br><br><input type="submit" value="Añadir" name="anadir">
         </form>
         <form action="ejer2.php" method="post">
             <h3>Hamburguesa</h3>
